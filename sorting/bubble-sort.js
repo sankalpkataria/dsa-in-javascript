e0051959cecd8d5a5054683d5c3db368cfa8c698
sankalp kataria <sankalpkataria@gmail.com>
2020-09-27 16:10:15 +0530

    bubble sort

---
+++ sorting/bubble-sort.js
@@ -0,0 +1,27 @@
+function swap(arr, i, j) {
+	let temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
+}
+
+/**
+ * @name bubbleSort
+ * @param {number[]} arr Input array
+ * @description Sort the given array using the bubble sort
+ * @summary For each iteration, compare the element with each subsequent element on right and 
+ *      check if the element on left is greater than the element on right,
+ *      If element on right is greater, Swap
+ * @Time complexity: - O(n^2)
+ * @returns {number[]} Sorted array
+ */
+function bubbleSort(arr) {
+	var arrLen = arr.length;
+	for (var i = 0; i < arrLen; i++) {
+		for (var j = 0; j < arrLen-i-1; j++) {
+			if (arr[j] > arr[j+1]) {
+				swap(arr, j, j+1);
+			}
+		}
+	}
+	return arr;
+}
